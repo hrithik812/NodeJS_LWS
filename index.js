4daf1d84555363f3c9747a98db1179e73a586bcf
hrithik812 <hrithik08.rudra@gmail.com>
2023-04-02 07:44:22 +0600

    First Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const {people,test,a:b}=require('./people');
+// console.log(global);
+
+// setTimeout(()=>{
+//     console.log('test');
+// },1000)
+const a=5;
+console.log(__dirname);
+console.log(__filename);
+console.log(people);
---
+++ people.js
@@ -0,0 +1,11 @@
+var people=['saakib','mashrafi'];
+const a=9;
+
+function test(){
+    console.log('test')
+}
+module.exports={
+    people,
+    a,
+    test
+};
